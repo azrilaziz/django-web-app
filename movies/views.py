61bdd402ea92d2ab2981df76b56cb9742fbf9583
acidsneaker <ctrlenter@gmail.com>
2019-05-26 01:29:21 +0000

    searchresults

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,16 @@ from django.contrib import messages
 from airtable import Airtable
 import os
 
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+             'Movies',
+             api_key=os.environ.get('AIRTABLE_API_KEY')
+             )
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'movies/movies_stuff.html')
+    #print(str(request.GET.get('query', '')))
+    user_query = str(request.GET.get('query', ''))
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({name}))")
+    stuff_for_frontend = {'search_result': search_result}
+    return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
 
